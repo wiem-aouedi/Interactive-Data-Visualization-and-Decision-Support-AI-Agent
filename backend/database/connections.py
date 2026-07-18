0b8529fd8b5c8dd9c772c640185e027c43e99620
Wiem Aouedi <wiem.aouadi@ept.ucar.tn>
2026-07-18 19:49:54 +0100

    modification in the connection file

--- backend/database/connections.py
+++ backend/database/connections.py
@@ -9,8 +9,12 @@ db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")  
 db_password = os.getenv("DB_PASSWORD")
+readonly_user = os.getenv("READONLY_DB_USER")
+readonly_password = os.getenv("READONLY_DB_PASSWORD")
 db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 engine = create_engine(db_url)
+readonly_db_url = f"postgresql://{readonly_user}:{readonly_password}@{db_host}:{db_port}/{db_name}"
+readonly_engine = create_engine(readonly_db_url)
 if __name__ == "__main__":
     try:
         connection = engine.connect()
@@ -18,3 +22,10 @@ if __name__ == "__main__":
         connection.close()
     except Exception as e:
         print("Error connecting to the database:", e)
+
+    try:
+        readonly_connection = readonly_engine.connect()
+        print("The read-only connection was successful!")
+        readonly_connection.close()
+    except Exception as e:
+        print("Error connecting with read-only user:", e)
